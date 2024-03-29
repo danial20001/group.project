95d77f058641e4e6e19dfa0875d5c2608131b3d1
danial <danyaalshahid01@gmail.com>
2024-03-29 12:40:40 +0000

    3rd commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,10 +2,15 @@ package com.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+
+    @FXML
+    private HBox cardLayout;
 
     @FXML
     protected void onHelloButtonClick() {
